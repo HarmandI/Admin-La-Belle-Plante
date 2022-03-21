9ffaa62e0ff0f1e143dc4ea45bd69497ee90a65b
HarmandI <isabelle44.74@gmail.com>
2022-03-21 13:04:19 +0100

    composant plantForm + model

--- src/app/modules/admin/components/formulaire/formulaire.component.ts
+++ src/app/modules/admin/components/formulaire/formulaire.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulaire',
   templateUrl: './formulaire.component.html',
-  styleUrls: ['./formulaire.component.scss']
+  styleUrls: ['./formulaire.component.scss'],
 })
 export class FormulaireComponent implements OnInit {
+  plantForm!: FormGroup;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onSubmit(){}
 }
+
