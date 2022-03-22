19d48cc995fa5c577ad61308c3a292890bc6199b
Vincent Ramiere <vincent.ramiere1@gmail.com>
2022-03-22 09:44:39 +0100

    modif inStock

--- src/app/modules/admin/models/plant.ts
+++ src/app/modules/admin/models/plant.ts
@@ -5,7 +5,7 @@ export class Plant {
     public name: string='',
     public price: number =1,
     public quantity: number= 0,
-    public instock: boolean= true,
+    public inStock: boolean= true,
     public category:Category,
     public urlPicture: string = "https//picsum.photos/id/18/200/300",
     public rating: number = 0,
